b9f7f317b3b2e53f43b86c1cfca1ab88aa050555
Andhika Prasandy Rachman <72144035+andhika-r@users.noreply.github.com>
2021-07-20 00:38:11 +0700

    major heuristic update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -95,9 +95,6 @@
                             <div class="col">
                                 <h3 class="mb-0">Daftar Arsip</h3>
                             </div>
-                            <div class="col text-right">
-                                <a href="#!" class="btn btn-sm btn-primary">See all</a>
-                            </div>
                         </div>
                     </div>
                     <div class="table-responsive">
